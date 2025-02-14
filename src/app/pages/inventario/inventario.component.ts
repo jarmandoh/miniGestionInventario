701ea801c71a583b06a220d52fa2f715a33195cb
janier hernandez <janier007@gmail.com>
2025-02-14 07:37:49 -0500

    dummy inventario

--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -15,23 +15,28 @@ import { NgClass, NgFor, NgIf } from '@angular/common';
   styleUrl: './inventario.component.css'
 })
 export class InventarioComponent {
-  productos: any[] = [];
+  productos: any[] = [
+    {
+      id: 1,
+      nombre: 'Producto 1',
+      precio: 100,
+      cantidad: 10,
+    },
+    {
+      id: 2,
+      nombre: 'Producto 2',
+      precio: 200,
+      cantidad: 20,
+    },
+  ];
   mensajeError: string = '';
 
   constructor(private apiService: ApiService, private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    /* if (!this.authService.isAuthenticated()) {
-      this.router.navigate(['/login']);
-    } */
-
     this.apiService.obtenerInventario().subscribe(
       (data) => {
         this.productos = data;
-      },
-      (error) => {
-        this.mensajeError = 'Error al cargar el inventario';
-      }
-    );
+      });
   }
 }
